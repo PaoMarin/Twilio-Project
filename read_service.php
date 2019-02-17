a91273f1d95b53aa13525c62749a3afc860ccde7
Joselyn Marín <paomg.9614@gmail.com>
2019-02-16 22:33:13 -0600

    Leer el servicio

---
+++ read_service.php
@@ -0,0 +1,20 @@
+<?php
+
+// Update the path below to your autoload.php,
+// see https://getcomposer.org/doc/01-basic-usage.md
+
+require __DIR__ . '/vendor/autoload.php';
+
+use Twilio\Rest\Client;
+
+// Find your Account Sid and Auth Token at twilio.com/console
+$sid = "ACf8d3619e2650cb1046800b000466f219";
+$token = "0149cd1c19766a7e2e64206aca54d5e7";
+$twilio = new Client($sid, $token);
+
+$services = $twilio->chat->v2->services
+                             ->read();
+
+foreach ($services as $record) {
+    print($record->friendlyName);
+}
